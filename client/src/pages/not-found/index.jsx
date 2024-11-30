c2cef5e1e6c850ab3432834ea35fa2a6b51babc8
yashbaseta <yashbaseta05@mail.com>
2024-11-30 16:31:42 +0530

    E-commerce

--- client/src/pages/not-found/index.jsx
+++ client/src/pages/not-found/index.jsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+
 function NotFound(params) {
+const navigate = useNavigate()
     return(
-        <div>page doesn't exist</div>
+        <div>page doesn't 
+            
+        <Button onClick={()=>navigate('/auth/login')}>R</Button>
+        </div>
     )
 }
 
