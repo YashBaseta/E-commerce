2a270e9993ba08daab6449e6e60c87fd21bffd88
yashbaseta <yashbaseta05@mail.com>
2024-11-30 16:56:14 +0530

    E-commerce

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import AuthLayout from './components/auth/layout.jsx'
+import { inject } from '@vercel/analytics';
+inject();
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
